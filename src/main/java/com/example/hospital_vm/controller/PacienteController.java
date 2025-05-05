bea291cc87520395dadcb1f8e8e11b412730180d
unknown <SSDD@PNAWSA04LC41500.duoc.acad>
2025-05-05 12:03:46 -0400

    Save ok

--- src/main/java/com/example/hospital_vm/controller/PacienteController.java
+++ src/main/java/com/example/hospital_vm/controller/PacienteController.java
@@ -4,7 +4,7 @@ import com.example.hospital_vm.model.Paciente;
 import com.example.hospital_vm.service.PacienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Optional;
-
+import java.net.URI;
 import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,53 +29,61 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/v1/pacientes")
 public class PacienteController {
 
-    @Autowired
-    private PacienteService pacienteService;
+    private final PacienteService pacienteService;
 
-    @GetMapping
-    public String inicio() {
-        return "Hola Mundo";
+    public PacienteController(PacienteService pacienteService){
+        this.pacienteService = pacienteService;
     }
+
+    
     
     @GetMapping("/listar-pacientes")
     public ResponseEntity<List<Paciente>> listar(){
-        List<Paciente> pacientes = pacienteService.findAll();
-        if(pacientes.isEmpty()){
-            return ResponseEntity.noContent().build();
+        try{
+            List<Paciente> pacientes = pacienteService.findAll();
+
+            return pacientes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(pacientes);
+
+            // if(pacientes.isEmpty()){
+            //     return ResponseEntity.noContent().build();
+            // }
+            //return ResponseEntity.ok(pacientes);
+        }catch(Exception ex){
+            return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.ok(pacientes);
     }
 
     @PostMapping
-    public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente){
-        Paciente pacienteNuevo = pacienteService.save(paciente);
+    public ResponseEntity<?> save(@RequestBody Paciente paciente){
+        try{
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteNuevo);
+            Paciente pacienteGuardado = pacienteService.save(paciente);
+            //
+
+            //Uri del nuevo recurso creado 
+            URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(pacienteGuardado.getId())
+                        .toUri();
+
+            return ResponseEntity
+                        .created(location)//Código 201 created
+                        .body(pacienteGuardado);
+                        
+
+        }catch(DataIntegrityViolationException e){
+            Map<String,String> error = new HashMap<>();
+            error.put("message","El email ya está registrado");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(error);//404
+        }
     }
-    
-    
-    
-    // @GetMapping("/{id}")
-    // public ResponseEntity<?> getPacientePorId(@PathVariable Integer id){
-    //       Optional<Paciente> paciente = pacienteService.getPacientePorId(id);
 
-    //       if(paciente.isPresent()){
-    //         //Respuesta exitosa con cabeceras personalizadas
-    //         return ResponseEntity.ok()
-    //                 .header("Correcto","Llamada correcto")
-    //                 .body(paciente.get());
-    //       }else{
-    //         //Respuesta de error con cuerpo personalizado
-    //         Map<String,String> errorBody = new HashMap<>();
-    //         errorBody.put("message","No se encontro un paciente con ese ID: " + id);
-    //         errorBody.put("status", "404");
-    //         errorBody.put("timestamp",LocalDateTime.now().toString());
+    // @PostMapping
+    // public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente){
+    //     Paciente pacienteNuevo = pacienteService.save(paciente);
 
-    //         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-    //                     .body(errorBody);
-    //       }
-
-    //       //Se usa <?> para pe
+    //     return ResponseEntity.status(HttpStatus.CREATED).body(pacienteNuevo);
     // }
     
     
